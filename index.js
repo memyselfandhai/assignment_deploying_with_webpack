a40b7127dc7442bf03537e4ce7698d387ab5a168
Lakshmi <lakshmi.nishthala@gmail.com>
2018-01-11 09:13:43 -0800

    Trying to fetch data from SWAPI

--- index.js
+++ index.js
@@ -4,5 +4,16 @@ import "./style.css";
 
 const Greeting = ({ name }) => <p className="greet">Hello, {name}!</p>;
 
-ReactDOM.render(<Greeting name="Reign" />, document.getElementById("root"));
-
+const ApiData = () => {
+  fetch("https://swapi.co/api/people/1")
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(json => {
+      // this.setState({ display: json });
+      //console.log(this);
+      return json;
+    });
+};
+ReactDOM.render(<ApiData />, document.getElementById("root"));
