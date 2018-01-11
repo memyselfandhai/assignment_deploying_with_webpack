88d407cdb93eeea4fdc10856c38c8aab183df727
Hai Nguyen <memyselfandhai@gmail.com>
2018-01-11 11:35:43 -0600

    creating first component

--- index.js
+++ index.js
@@ -4,16 +4,4 @@ import "./style.css";
 
 const Greeting = ({ name }) => <p className="greet">Hello, {name}!</p>;
 
-const ApiData = () => {
-  fetch("https://swapi.co/api/people/1")
-    .then(response => {
-      console.log(response);
-      return response.json();
-    })
-    .then(json => {
-      // this.setState({ display: json });
-      //console.log(this);
-      return json;
-    });
-};
-ReactDOM.render(<ApiData />, document.getElementById("root"));
+ReactDOM.render(<Greeting name="Vlad" />, document.getElementById("root"));
